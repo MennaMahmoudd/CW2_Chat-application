99fd0cc33e3a469d5be06b352b5809472dd15cf5
Menna <ME2101695@TKH.EDU.EG>
2024-04-21 20:47:47 +0200

    modified server code

--- server.cpp
+++ server.cpp
@@ -5,68 +5,10 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
-#include <openssl/conf.h>
-#include <openssl/evp.h>
-#include <openssl/err.h>
 
 using namespace std;
 
-void handleErrors() {
-    ERR_print_errors_fp(stderr);
-    abort();
-}
-
-int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key, unsigned char *iv, unsigned char *plaintext) {
-    EVP_CIPHER_CTX *ctx;
-    int len;
-    int plaintext_len;
-
-    if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
-
-    if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-        handleErrors();
-
-    if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
-        handleErrors();
-    plaintext_len = len;
-
-    if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len)) handleErrors();
-    plaintext_len += len;
-
-    EVP_CIPHER_CTX_free(ctx);
-
-    return plaintext_len;
-}
-
-int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigned char *iv, unsigned char *ciphertext) {
-    EVP_CIPHER_CTX *ctx;
-    int len;
-    int ciphertext_len;
-
-    if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
-
-    if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-        handleErrors();
-
-    if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
-        handleErrors();
-    ciphertext_len = len;
-
-    if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
-    ciphertext_len += len;
-
-    EVP_CIPHER_CTX_free(ctx);
-
-    return ciphertext_len;
-}
-
-bool initialize() {
-    OpenSSL_add_all_algorithms();
-    ERR_load_crypto_strings();
-    return true;
-}
-
-void InteractWithClient(int clientSocket, int* clients, int& clientCount, unsigned char *key, unsigned char *iv) {
+void InteractWithClient(int clientSocket, int* clients, int& clientCount) {
     cout << "Client connected." << endl;
 
     unsigned char buffer[4096];
@@ -79,21 +21,11 @@ void InteractWithClient(int clientSocket, int* clients, int& clientCount, unsign
             break;
         }
 
-        int plaintext_len;
-        unsigned char plaintext[4096];
-        plaintext_len = decrypt(buffer, bytesrecvd, key, iv, plaintext);
-        plaintext[plaintext_len] = '\0';
-
-        cout << "Encrypted message from client: " << buffer << endl;
-        cout << "Decrypted message from client: " << plaintext << endl;
+        cout << "Message from client: " << buffer << endl;
 
         for (int i = 0; i < clientCount; ++i) {
             if (clients[i] != clientSocket) {
-                int ciphertext_len;
-                unsigned char ciphertext[4096];
-                // Encrypt the received message before sending to other clients
-                ciphertext_len = encrypt(plaintext, plaintext_len, key, iv, ciphertext);
-                send(clients[i], ciphertext, ciphertext_len, 0);
+                send(clients[i], buffer, bytesrecvd, 0);
             }
         }
     }
@@ -112,11 +44,6 @@ void InteractWithClient(int clientSocket, int* clients, int& clientCount, unsign
 }
 
 int main() {
-    if (!initialize()) {
-        cout << "Initialization failed." << endl;
-        return 1;
-    }
-
     cout << "Server program." << endl;
 
     int listenSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -153,10 +80,6 @@ int main() {
     int clients[MAX_CLIENTS];
     int clientCount = 0;
 
-    // AES key and IV (Initialization Vector)
-    unsigned char key[] = "01234567890123456789012345678901";
-    unsigned char iv[] = "0123456789012345";
-
     while (true) {
         int clientSocket = accept(listenSocket, nullptr, nullptr);
         if (clientSocket == -1) {
@@ -171,7 +94,7 @@ int main() {
         }
 
         clients[clientCount++] = clientSocket;
-        thread t1(InteractWithClient, clientSocket, clients, ref(clientCount), key, iv);
+        thread t1(InteractWithClient, clientSocket, clients, ref(clientCount));
         t1.detach();
     }
 
